6eabb39da8510a5a216cc0ee8da50859aae9e4bf
atltt <atltt73@gmail.com>
2018-10-11 15:49:00 -0400

    will this fix #37

--- lib/main/minskylink.ts
+++ lib/main/minskylink.ts
@@ -1,10 +1,6 @@
 // import {StatusBar} from "atom/status-bar"
 
-<<<<<<< HEAD
 console.log(String("Loading Minsky Link"))
 
 //Unsure what loading object should be
 //atom.onload = cursorListerner()
-=======
-console.log(String("Loading Minsky Link"));
->>>>>>> origin/master
