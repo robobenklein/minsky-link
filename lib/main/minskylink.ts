0228ef50e924e7e50932558bc5f41302609c2362
Rex <rexrascerx@gmail.com>
2018-10-17 18:44:18 -0400

    Added an export defualt.

--- lib/main/minskylink.ts
+++ lib/main/minskylink.ts
@@ -1,3 +1,30 @@
 // import {StatusBar} from "atom/status-bar"
 
-console.log(String("Loading Minsky Link"));
+console.log(String("Loading Minsky Link"))
+
+import {CompositeDisposable} from "atom";
+
+export default {
+
+  subscriptions:CompositeDisposable,
+  activate() {
+
+    // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+    this.subscriptions = new CompositeDisposable();
+
+    // Register command that toggles this view
+    this.subscriptions.add(atom.commands.add('atom-workspace', {
+      'my-package:toggle': () => this.toggle()
+    }));
+  },
+
+  deactivate() {
+    this.subscriptions.dispose();
+  },
+
+  serialize() {},
+
+  toggle():void {
+    console.log('MyPackage was toggled!');
+  }
+};
