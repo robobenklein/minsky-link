709e431ca3b57829d38aeb6e9f361a5a3f933a83
Rex <rexrascerx@gmail.com>
2018-10-19 13:34:14 -0400

    prettiered

--- lib/main/minskylink.ts
+++ lib/main/minskylink.ts
@@ -1,4 +1,5 @@
-import {CompositeDisposable} from "atom";
+import { CompositeDisposable } from "atom";
+//import {MenuManager} from "atom";
 
 let subscriptions: CompositeDisposable | undefined;
 
@@ -7,22 +8,24 @@ export async function activate() {
   subscriptions = new CompositeDisposable();
 
   // This adds the Active Command to our list of commands in Atom
-  subscriptions.add(atom.commands.add('atom-workspace', {
-    'minsky:speaks': () => speaks()
-  }));
-};
+  subscriptions.add(
+    atom.commands.add("atom-workspace", {
+      "minsky:speaks": () => speaks()
+    })
+  );
+}
 
 // For when Atom closes, this is what to do.
 export function deactivate() {
   if (subscriptions) subscriptions.dispose();
-};
+}
 
 export function serialize() {
-  return undefined
-};
+  return undefined;
+}
 
 // This is an active command function. You can add more in the
 // activate function.
-export function speaks():void {
-  console.log('Minsky was asked to Speak!');
-};
+export function speaks(): void {
+  console.log("Minsky was asked to Speak!");
+}
