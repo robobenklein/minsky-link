9ef51ffb50cab82539559689c2d4589d5087f4ea
Ben Klein <robobenklein@gmail.com>
2018-10-23 13:36:08 -0400

    WIP Got the wrong thing done lol
    Intended to get hover listener, but instead got issue tag recognizer

--- dist/main/minskylink.js
+++ dist/main/minskylink.js
@@ -1,4 +1,20 @@
 "use strict";
-// import {StatusBar} from "atom/status-bar"
+Object.defineProperty(exports, "__esModule", { value: true });
+require("atom");
 console.log(String("Loading Minsky Link"));
+// HOVER LISTENER GH40
+// search for issue tags in a TextBuffer and apply markers to them
+function findIssueTags(textToSearch) {
+    var regex1 = new RegExp(/( GH([0-9]+))/);
+    textToSearch.scan(regex1, (scanResult) => {
+        console.log("Found issue tag: " + scanResult.matchText);
+    });
+}
+// const subscriptions = new CompositeDisposable();
+atom.workspace.observeTextEditors(editor => {
+    console.log("Opening editor: " + editor.getLongTitle());
+    console.log("Running scan on " + editor.getLongTitle());
+    findIssueTags(editor.getBuffer());
+    // TODO place hook on the buffer being edited:
+});
 //# sourceMappingURL=minskylink.js.map
