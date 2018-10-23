8aa9555846c86662b6f883724aef961d439a9b3c
Ben Klein <robobenklein@gmail.com>
2018-10-23 13:36:08 -0400

    WIP Got the wrong thing done lol

--- lib/main/minskylink.ts
+++ lib/main/minskylink.ts
@@ -1,6 +1,24 @@
-// import {StatusBar} from "atom/status-bar"
+import 'atom';
+import {TextBuffer} from 'atom';
 
 console.log(String("Loading Minsky Link"));
 
-//Unsure what loading object should be
-//atom.onload = cursorListerner()
+// HOVER LISTENER GH40
+
+// search for issue tags in a TextBuffer and apply markers to them
+function findIssueTags(textToSearch: TextBuffer) {
+  var regex1 = new RegExp( /( GH([0-9]+))/ );
+  textToSearch.scan(regex1, (scanResult) => {
+    console.log("Found issue tag: " + scanResult.matchText);
+  });
+}
+
+// const subscriptions = new CompositeDisposable();
+
+atom.workspace.observeTextEditors(editor => {
+  console.log("Opening editor: " + editor.getLongTitle());
+  console.log("Running scan on " + editor.getLongTitle());
+  findIssueTags(editor.getBuffer());
+
+  // TODO place hook on the buffer being edited:
+});
