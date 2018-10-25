7af5f020f48aa61c27d4cb0bedf2d613e68f83aa
Ben Klein <robobenklein@gmail.com>
2018-10-25 19:35:42 -0400

    Working tag-finding on changes

--- dist/main/minskylink.js
+++ dist/main/minskylink.js
@@ -10,17 +10,28 @@ function findIssueTags(textToSearch) {
         var issue_number = parseInt(scanResult.match[2]);
         console.log("Found issue tag: " + scanResult.matchText);
         console.log("Creating marker on " + scanResult.range);
-        var new_marker = textToSearch.markRange(scanResult.range, {
+        var new_marker = textToSearch.markBufferRange(scanResult.range, {
             invalidate: 'touch'
         });
-        console.log("Created marker for issue #" + issue_number + ": " + new_marker.getStartPosition());
+        console.log("Created marker for issue #" + issue_number + ": " + new_marker.getStartBufferPosition);
+        var new_decoration = textToSearch.decorateMarker(new_marker, {
+            type: "highlight",
+            class: "minskylink_issue_tag"
+        });
+        console.log("Decorated #" + issue_number + " with " + new_decoration);
     });
 }
 // const subscriptions = new CompositeDisposable();
 atom.workspace.observeTextEditors(editor => {
     console.log("Opening editor: " + editor.getLongTitle());
     console.log("Running scan on " + editor.getLongTitle());
-    findIssueTags(editor.getBuffer());
+    findIssueTags(editor);
     // TODO place hook on the buffer being edited:
+    editor.onDidStopChanging(event_editorchanged => {
+        // event_editorchanged.changes.forEach(text_change => {
+        //   // check if the change range is in any of our issue tags.
+        // })
+        findIssueTags(editor);
+    });
 });
 //# sourceMappingURL=minskylink.js.map
--- lib/main/minskylink.ts
+++ lib/main/minskylink.ts
@@ -1,21 +1,26 @@
 import "atom";
-import { TextBuffer } from "atom";
+import { TextEditor } from "atom";
 
 console.log(String("Loading Minsky Link"));
 
 // HOVER LISTENER GH40
 
 // search for issue tags in a TextBuffer and apply markers to them
-function findIssueTags(textToSearch: TextBuffer) {
+function findIssueTags(textToSearch: TextEditor) {
   var regex1 = new RegExp(/( GH([0-9]+))/);
   textToSearch.scan(regex1, scanResult => {
     var issue_number: number = parseInt(scanResult.match[2]);
     console.log("Found issue tag: " + scanResult.matchText);
     console.log("Creating marker on " + scanResult.range);
-    var new_marker = textToSearch.markRange(scanResult.range, {
+    var new_marker = textToSearch.markBufferRange(scanResult.range, {
       invalidate: 'touch'
     });
-    console.log("Created marker for issue #" + issue_number + ": " + new_marker.getStartPosition());
+    console.log("Created marker for issue #" + issue_number + ": " + new_marker.getStartBufferPosition);
+    var new_decoration =  textToSearch.decorateMarker(new_marker, {
+      type: "highlight",
+      class: "minskylink_issue_tag"
+    });
+    console.log("Decorated #" + issue_number + " with " + new_decoration);
   });
 }
 
@@ -24,7 +29,13 @@ function findIssueTags(textToSearch: TextBuffer) {
 atom.workspace.observeTextEditors(editor => {
   console.log("Opening editor: " + editor.getLongTitle());
   console.log("Running scan on " + editor.getLongTitle());
-  findIssueTags(editor.getBuffer());
+  findIssueTags(editor);
 
   // TODO place hook on the buffer being edited:
+  editor.onDidStopChanging(event_editorchanged => {
+    // event_editorchanged.changes.forEach(text_change => {
+    //   // check if the change range is in any of our issue tags.
+    // })
+    findIssueTags(editor);
+  });
 });
