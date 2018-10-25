b6e5231ca0558601759bdf01c64d1209fb2dd838
Ben Klein <robobenklein@gmail.com>
2018-10-25 19:50:38 -0400

    Do not wait for activation to load plugin

--- dist/main/minskylink.js
+++ dist/main/minskylink.js
@@ -36,25 +36,11 @@ atom.workspace.observeTextEditors(editor => {
     });
 });
 let subscriptions;
-async function activate() {
-    // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
-    subscriptions = new atom_1.CompositeDisposable();
-    // This adds the Active Command to our list of commands in Atom
-    subscriptions.add(atom.commands.add("atom-workspace", {
-        "minsky:speaks": () => speaks()
-    }));
-}
-exports.activate = activate;
-// For when Atom closes, this is what to do.
-function deactivate() {
-    if (subscriptions)
-        subscriptions.dispose();
-}
-exports.deactivate = deactivate;
-function serialize() {
-    return undefined;
-}
-exports.serialize = serialize;
+subscriptions = new atom_1.CompositeDisposable();
+// This adds the Active Command to our list of commands in Atom
+subscriptions.add(atom.commands.add("atom-workspace", {
+    "minsky:speaks": () => speaks()
+}));
 // This is an active command function. You can add more in the
 // activate function.
 function speaks() {
--- lib/main/minskylink.ts
+++ lib/main/minskylink.ts
@@ -42,26 +42,14 @@ atom.workspace.observeTextEditors(editor => {
 
 let subscriptions: CompositeDisposable | undefined;
 
-export async function activate() {
-  // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
-  subscriptions = new CompositeDisposable();
+subscriptions = new CompositeDisposable();
 
   // This adds the Active Command to our list of commands in Atom
-  subscriptions.add(
-    atom.commands.add("atom-workspace", {
-      "minsky:speaks": () => speaks()
-    })
-  );
-}
-
-// For when Atom closes, this is what to do.
-export function deactivate() {
-  if (subscriptions) subscriptions.dispose();
-}
-
-export function serialize() {
-  return undefined;
-}
+subscriptions.add(
+  atom.commands.add("atom-workspace", {
+    "minsky:speaks": () => speaks()
+  })
+);
 
 // This is an active command function. You can add more in the
 // activate function.
