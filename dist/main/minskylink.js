0cc54cce530213222dc152fef5bcb919bbe3ea3f
Ben Klein <robobenklein@gmail.com>
2018-10-29 17:46:04 -0400

    Fixes multiple issue tags in one editor

--- dist/main/minskylink.js
+++ dist/main/minskylink.js
@@ -4,14 +4,30 @@ require("atom");
 const atom_1 = require("atom");
 console.log(String("Loading Minsky Link"));
 // HOVER LISTENER GH40
+// ISSUE TAG FINDER GH33
 // search for issue tags in a TextBuffer and apply markers to them
-function findIssueTags(textToSearch) {
-    var regex1 = new RegExp(/(GH([0-9]+))/);
+function findIssueTags(textToSearch, issuetaglayer) {
+    var regex1 = new RegExp(/(GH([0-9]+))/, "gm");
     textToSearch.scan(regex1, scanResult => {
         var issue_number = parseInt(scanResult.match[2]);
         console.log("Found issue tag: " + scanResult.matchText);
+        // check if there already exists a marker on this layer:
+        var existing_markers = issuetaglayer.findMarkers({
+            intersectsBufferRange: scanResult.range
+        });
+        for (var i = 0; i < existing_markers.length; i++) {
+            if (existing_markers[i].isValid() == true) {
+                // still is valid? don't bother
+                console.log("Issue tag #" + issue_number + " already known: " + existing_markers[i].getBufferRange());
+                return;
+            }
+            else {
+                // destroy the invalid marker:
+                existing_markers[i].destroy();
+            }
+        }
         console.log("Creating marker on " + scanResult.range);
-        var new_marker = textToSearch.markBufferRange(scanResult.range, {
+        var new_marker = issuetaglayer.markBufferRange(scanResult.range, {
             invalidate: "touch"
         });
         console.log("Created marker for issue #" +
@@ -24,18 +40,20 @@ function findIssueTags(textToSearch) {
         });
         console.log("Decorated #" + issue_number + " with " + new_decoration);
     });
+    console.log("issuetaglayer currently has " + issuetaglayer.getMarkerCount() + " items!");
 }
 atom.workspace.observeTextEditors(editor => {
     console.log("Opening editor: " + editor.getLongTitle());
     console.log("Running scan on " + editor.getLongTitle());
-    findIssueTags(editor);
+    var issuetaglayer = editor.addMarkerLayer({});
+    findIssueTags(editor, issuetaglayer);
     // TODO place hook on the buffer being edited:
     editor.onDidStopChanging(event_editorchanged => {
         event_editorchanged.changes.forEach(text_change => {
             // check if the change range is in any of our issue tags.
             console.log("Changed text: " + text_change.oldRange);
         });
-        findIssueTags(editor);
+        findIssueTags(editor, issuetaglayer);
     });
 });
 let subscriptions;
--- lib/main/minskylink.ts
+++ lib/main/minskylink.ts
@@ -1,19 +1,38 @@
 import "atom";
 import { CompositeDisposable } from "atom";
 import { TextEditor } from "atom";
+import { DisplayMarkerLayer } from "atom";
 
 console.log(String("Loading Minsky Link"));
 
 // HOVER LISTENER GH40
 
+// ISSUE TAG FINDER GH33
+
 // search for issue tags in a TextBuffer and apply markers to them
-function findIssueTags(textToSearch: TextEditor) {
-  var regex1 = new RegExp(/(GH([0-9]+))/);
+function findIssueTags(textToSearch: TextEditor, issuetaglayer: DisplayMarkerLayer) {
+  var regex1 = new RegExp(/(GH([0-9]+))/, "gm");
+
   textToSearch.scan(regex1, scanResult => {
     var issue_number: number = parseInt(scanResult.match[2]);
     console.log("Found issue tag: " + scanResult.matchText);
+    // check if there already exists a marker on this layer:
+    var existing_markers = issuetaglayer.findMarkers({
+      intersectsBufferRange: scanResult.range
+    })
+    for (var i: number = 0; i < existing_markers.length; i++) {
+      if (existing_markers[i].isValid() == true) {
+        // still is valid? don't bother
+        console.log("Issue tag #" + issue_number + " already known: " + existing_markers[i].getBufferRange())
+        return
+      }
+      else {
+        // destroy the invalid marker:
+        existing_markers[i].destroy();
+      }
+    }
     console.log("Creating marker on " + scanResult.range);
-    var new_marker = textToSearch.markBufferRange(scanResult.range, {
+    var new_marker = issuetaglayer.markBufferRange(scanResult.range, {
       invalidate: "touch"
     });
     console.log(
@@ -28,12 +47,17 @@ function findIssueTags(textToSearch: TextEditor) {
     });
     console.log("Decorated #" + issue_number + " with " + new_decoration);
   });
+
+  console.log("issuetaglayer currently has " + issuetaglayer.getMarkerCount() + " items!")
 }
 
 atom.workspace.observeTextEditors(editor => {
   console.log("Opening editor: " + editor.getLongTitle());
   console.log("Running scan on " + editor.getLongTitle());
-  findIssueTags(editor);
+
+  var issuetaglayer: DisplayMarkerLayer = editor.addMarkerLayer({});
+
+  findIssueTags(editor, issuetaglayer);
 
   // TODO place hook on the buffer being edited:
   editor.onDidStopChanging(event_editorchanged => {
@@ -41,7 +65,7 @@ atom.workspace.observeTextEditors(editor => {
       // check if the change range is in any of our issue tags.
       console.log("Changed text: " + text_change.oldRange);
     });
-    findIssueTags(editor);
+    findIssueTags(editor, issuetaglayer);
   });
 });
 
