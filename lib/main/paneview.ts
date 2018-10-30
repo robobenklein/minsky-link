4bf149b922ddc4a5f626608dab246800f14683a5
Rex <jbueckman+git@gmail.com>
2018-10-30 12:05:30 -0400

    More parts to the problem

--- lib/main/paneview.ts
+++ lib/main/paneview.ts
@@ -1,17 +1,20 @@
 import {getGitHubIssue} from "../github/github_issue"
 
-export default class MyPackageView {
+export default class paneview {
 
-  constructor(serializedState) {
+  constructor(url) {
     // Create root element
     this.element = document.createElement('div');
-    this.element.classList.add('my-package');
+    this.element.classList.add('Minsky');
 
     // Create message element
-    const message = document.createElement('div');
-    message.textContent = 'The MyPackage package is Alive! It\'s ALIVE!';
-    message.classList.add('message');
-    this.element.appendChild(message);
+    //const content = document.createElement('div');
+    const currentIssue = getGitHubIssue(,, url.substr(url.lastIndexOf("/") + 1));
+    for (let i of currentIssue.getAllComments()) {
+      const message = document.createElement('div');
+      message.textContent = i.body;
+      this.element.appendChild(message);
+    }
   }
 
   // Returns an object that can be retrieved when package is activated
