a13bfa1c87d236df3efc8701dafc55224f218d97
atltt <atltt73@gmail.com>
2018-11-06 22:42:19 -0500

    #58, outlining the frame for it

---
+++ dist/github/githubAuth.js
@@ -0,0 +1,17 @@
+//https://www.npmjs.com/package/@octokit/rest#authentication
+
+const GitHub = require('@octokit/rest');
+
+class githubAuth {
+  //GitHub.authenticate('oauth', process.)
+}
+
+exports.githubAuth = githubAuth;
+
+/*
+octokit.authenticate({
+  type: 'oauth',
+  key: 'client_id',
+  secret: 'client_secret'
+})
+*/
