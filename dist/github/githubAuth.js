2f47ca9694070e7962924a4e3174e9e87012206c
atltt <atltt73@gmail.com>
2018-11-07 21:46:47 -0500

    #58, another basic outline of what it needs to be

--- dist/github/githubAuth.js
+++ dist/github/githubAuth.js
@@ -1,17 +1,21 @@
 //https://www.npmjs.com/package/@octokit/rest#authentication
-
+"use strict";
 const GitHub = require('@octokit/rest');
+const user = require("../git-interface/user");
 
+//Will fix later. 
 class githubAuth {
   //GitHub.authenticate('oauth', process.)
+  var keyinp = readline();
+
+  var newUser = user.User();
+
+  GitHub.authenticate({
+  type: 'oauth',
+  key: newUser,
+  secret: keyinp
+  })
+
 }
 
 exports.githubAuth = githubAuth;
-
-/*
-octokit.authenticate({
-  type: 'oauth',
-  key: 'client_id',
-  secret: 'client_secret'
-})
-*/
